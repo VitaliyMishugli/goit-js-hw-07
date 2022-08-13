52891914228660dd2e3555d04090df9b017650d2
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-13 22:23:08 +0100

    Add first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const makeGalleryItemMarkup = ({ original, preview, description }) => {
+  return ` 
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+};
+
+// console.log(makeGalleryItemMarkup(galleryItems));
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join("");
+// console.log(makeGalleryMarkup);
+gallery.insertAdjacentHTML("beforeend", makeGalleryMarkup);
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  //Створюємо модалку для одного зображення
+  const instance = basicLightbox.create(`
+      <img src="${e.target.src}" width="800" height="600">
+  `);
+
+  instance.show();
+  if (instance.visible()) {
+    window.addEventListener("keyup", (e) => {
+      console.log(e.code);
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    });
+  }
+});
+
+
